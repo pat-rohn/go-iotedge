3a80b431f39f3bfa642ecb7fa3d0cafed2cd77e9
schusterlein <schusterklein@live.com>
2021-09-19 21:32:31 +0200

    Fixed config

--- cmd/IoTServer/main.go
+++ cmd/IoTServer/main.go
@@ -52,13 +52,13 @@ func main() {
 
 func startServer() error {
 
-	viper.SetDefault("db.Name", "plottydb")
-	viper.SetDefault("db.IPOrPath", "localhost")
-	viper.SetDefault("db.UsePostgres", true)
-	viper.SetDefault("db.User", "user")
-	viper.SetDefault("db.Password", "password")
-	viper.SetDefault("db.Port", 5432)
-	viper.SetDefault("db.TableName", "measurements1")
+	viper.SetDefault("DbConfig.Name", "plottydb")
+	viper.SetDefault("DbConfig.IPOrPath", "localhost")
+	viper.SetDefault("DbConfig.UsePostgres", true)
+	viper.SetDefault("DbConfig.User", "user")
+	viper.SetDefault("DbConfig.Password", "password")
+	viper.SetDefault("DbConfig.Port", 5432)
+	viper.SetDefault("DbConfig.TableName", "measurements1")
 	viper.SetDefault("Port", 3004)
 
 	viper.SetConfigName("iot")
@@ -70,21 +70,24 @@ func startServer() error {
 	pathToConfig := dirname + "/.iotserver"
 	viper.AddConfigPath(pathToConfig)
 	viper.AddConfigPath(".")
-	if err = viper.ReadInConfig(); err != nil {
-		log.Warnf(fmt.Sprintf("no config file found: %w", err))
-
-		if err := os.Mkdir(pathToConfig, 0755); err != nil {
-			log.Fatal(fmt.Sprintf("Creating config folder failed: %w", err))
-		}
-		err = viper.SafeWriteConfig()
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Storing default config failed: %w", err))
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Warnf(fmt.Sprintf("no config file found: %v", err))
+			if err := os.Mkdir(pathToConfig, 0755); err != nil {
+				log.Fatal(fmt.Sprintf("Creating config folder failed: %v", err))
+			}
+			err = viper.SafeWriteConfig()
+			if err != nil {
+				log.Fatal(fmt.Sprintf("Storing default config failed: %v", err))
+			}
+		} else {
+			log.Fatal(fmt.Sprintf("Loading config failed: %v", err))
 		}
 	}
 
 	type config struct {
-		Port int
-		db   timeseries.DBConfig
+		Port     int
+		DbConfig timeseries.DBConfig
 	}
 
 	var iotConfig config
@@ -93,6 +96,7 @@ func startServer() error {
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w ", err))
 	}
-	iot := iotedge.New(iotConfig.db, iotConfig.Port)
+	fmt.Printf("%+v", iotConfig)
+	iot := iotedge.New(iotConfig.DbConfig, iotConfig.Port)
 	return iot.StartSensorServer()
 }
